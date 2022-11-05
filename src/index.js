46f950cb600ff5961607a94ae71bc0046ae18907
Douglas Rochak <douglas.srochak@gmail.com>
2022-11-05 16:25:05 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { v4:uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json())
+
+const customers = []
+
+app.get('/', (request, response) => {
+  
+  return response.json('Alo');
+})
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    name,
+    cpf,
+    statement: []
+  })
+  
+  return response.status(201).send();
+})
+
+app.listen(3333);
