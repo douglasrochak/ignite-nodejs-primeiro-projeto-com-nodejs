3683453d4c33e1f3011a35ac15b22b539c536bb0
Douglas Rochak <douglas.srochak@gmail.com>
2022-11-07 19:16:49 -0300

    aula middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ app.use(express.json())
 
 const customers = []
 
+// Middleware
+
+function verifyIfExistsAccountCPF(request, response, next){
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  
+  if(!customer){
+    return response.status(400).json({error: 'Customer not found'});
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.get('/customers', (request, response) => {
   
   return response.json({customers});
@@ -31,12 +47,11 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 })
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find((customer) => customer.cpf === cpf);
-  if(!customer){
-    return response.status(400).json({error: 'Customer not found'});
-  }
+// app.use(verifyIfExistsAccountCPF);
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
   return response.json(customer.statement);
 })
 
