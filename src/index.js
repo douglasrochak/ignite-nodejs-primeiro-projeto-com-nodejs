d9f9e9346a8152d240820a44b3e3b9c2e8c28e7b
Douglas Rochak <douglas.srochak@gmail.com>
2022-11-13 13:46:51 -0300

    aula listar extrato bancario por data

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const { v4:uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const customers = []
+const customers = [];
 
 // Middleware
 
-function verifyIfExistsAccountCPF(request, response, next){
+function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
 
   const customer = customers.find((customer) => customer.cpf === cpf);
-  
-  if(!customer){
-    return response.status(400).json({error: 'Customer not found'});
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
   }
 
   request.customer = customer;
@@ -23,11 +23,11 @@ function verifyIfExistsAccountCPF(request, response, next){
   return next();
 }
 
-function getBalance(statement){
+function getBalance(statement) {
   const balance = statement.reduce((acc, operation) => {
-    if(operation.type === 'credit'){
+    if (operation.type === "credit") {
       return acc + operation.amount;
-    }else {
+    } else {
       return acc - operation.amount;
     }
   }, 0);
@@ -35,39 +35,55 @@ function getBalance(statement){
   return balance;
 }
 
-app.get('/customers', (request, response) => {
-  
-  return response.json({customers});
-})
+app.get("/customers", (request, response) => {
+  return response.json({ customers });
+});
 
-app.post('/account', (request, response) => {
+app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf )
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-  if(customerAlreadyExists){
-    return response.status(400).json({error: 'Customer already exists'});
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
   }
 
   customers.push({
     id: uuidv4(),
     name,
     cpf,
-    statement: []
-  })
-  
+    statement: [],
+  });
+
   return response.status(201).send();
-})
+});
 
 // app.use(verifyIfExistsAccountCPF);
 
-app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
   return response.json(customer.statement);
-})
+});
 
-app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() === dateFormat.toDateString()
+  );
+
+  return response.json(statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   const { description, amount } = request.body;
 
   const { customer } = request;
@@ -76,34 +92,34 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     description,
     amount,
     created_at: new Date(),
-    type: 'credit'
-  }
+    type: "credit",
+  };
 
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
-})
+});
 
-app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   const { amount } = request.body;
 
   const { customer } = request;
 
   const balance = getBalance(customer.statement);
 
-  if(balance < amount) {
-    return response.status(400).json({error: 'Insufficient balance'})
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient balance" });
   }
 
   const statementOperation = {
     amount,
     created_at: new Date(),
-    type: 'debit'
-  }
+    type: "debit",
+  };
 
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
-})
+});
 
 app.listen(3333);
